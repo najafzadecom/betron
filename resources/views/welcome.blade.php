811e50aa94a591e9166b586b6272a0bce10c9b27
e1sndev <111046079+e1sndev@users.noreply.github.com>
2026-03-31 17:29:41 +0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Express Bank - Coming Soon</title>
+        <title>{{ config('app.name', 'Betron') }} - Coming Soon</title>
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
         <style>
