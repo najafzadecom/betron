17786e34341bacb4cdfdce4c7d44c22af6825ad0
e1sndev <111046079+e1sndev@users.noreply.github.com>
2026-03-31 17:28:48 +0400

    Update footer.blade.php

--- resources/views/admin/parts/footer.blade.php
+++ resources/views/admin/parts/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar navbar-sm navbar-footer border-top">
     <div class="container-fluid">
-        <span>© {{ date('Y') }} <a href="#">Bank Express</a></span>
+        <span>© {{ date('Y') }} <a href="#">{{ config('app.name', 'Betron') }}</a></span>
     </div>
 </div>
