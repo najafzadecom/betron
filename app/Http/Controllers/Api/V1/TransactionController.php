4d704542450fd9023b2b7caecc3f5823a92a029a
e1sndev <111046079+e1sndev@users.noreply.github.com>
2026-04-09 22:47:35 +0400

    Update TransactionController.php

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -47,7 +47,7 @@ class TransactionController extends BaseController
                     throw new RuntimeException($cashevoResult['message'] ?? 'Cashevo transaction failed');
                 }
 
-                $banka = $cashevoResult;
+                $banka = $cashevoResult['data'];
 
                 var_dump($banka);
                 die();
