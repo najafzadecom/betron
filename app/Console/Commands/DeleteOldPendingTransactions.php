c0a0b6ceb7e627cbdcd44881fcade1ce4dac9a4f
e1sndev <111046079+e1sndev@users.noreply.github.com>
2026-04-20 17:04:15 +0400

    Update DeleteOldPendingTransactions.php

--- app/Console/Commands/DeleteOldPendingTransactions.php
+++ app/Console/Commands/DeleteOldPendingTransactions.php
@@ -28,7 +28,7 @@ class DeleteOldPendingTransactions extends Command
      */
     public function handle()
     {
-        $thirtyMinutesAgo = Carbon::now('Europe/Istanbul')->subMinutes(10);
+        $thirtyMinutesAgo = Carbon::now('Europe/Istanbul')->subMinutes(env('PENDING_TRANSACTION_EXPIRE_TIME', 10));
 
         $transactions = Transaction::query()
             ->where('status', TransactionStatus::Pending)
